a9d9b37daccf230ce1d987bda9aaf8383abb1690
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-09-08 23:34:18 +0530

    Km to miles using cmd_line_args

---
+++ KmtoMiles.java
@@ -0,0 +1,10 @@
+//kilometers to miles
+//miles=km*0.621371
+class KmtoMiles{
+	public static void main(String args[]){
+		double km=Double.parseDouble(args[0]);
+		double miles=km*0.621371;
+		System.out.println("Miles: "+miles);
+	}
+
+}
