24fa536dd898389b4a9d921e70b108192b50377a
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-09-08 22:23:33 +0530

    using_cmd_line_args

---
+++ Pythagoras.java
@@ -0,0 +1,11 @@
+class Pythagoras{
+	public static void main(String args[]){
+		System.out.println("Enter Perpendicular and Base values");
+		double perpendicular=Double.parseDouble(args[0]);
+		double base=Double.parseDouble(args[1]);
+		double c;
+		c=((perpendicular*perpendicular)+(base*base));
+		double hyp=Math.sqrt(c);
+		System.out.println(hyp);
+	}
+}
