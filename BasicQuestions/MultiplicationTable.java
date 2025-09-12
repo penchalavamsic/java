bda067ea63bde89acd4431f46ac2c113f864cad4
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-09-12 11:09:34 +0530

    Sum of Numbers & Multiplication table

---
+++ BasicQuestions/MultiplicationTable.java
@@ -0,0 +1,12 @@
+//Multiplication table 
+import java.util.*;
+class MultiplicationTable{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num=sc.nextInt();
+		for(int i=1;i<=10;i++){
+			System.out.println(num+"*"+i+"="+num*i);
+		}
+	}
+}
---
+++ BasicQuestions/SumOfNumbers.java
@@ -0,0 +1,9 @@
+//Sum of numbers 
+//n*(n+1)/2  1-100
+class SumOfNumbers{
+	public static void main(String args[]){
+		int n=100;
+		int sum=n*(n+1)/2;	
+		System.out.println(sum);
+	}
+}
