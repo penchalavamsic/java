5ce1b5f397472151951f27df3be23472620bd876
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-09-14 19:57:49 +0530

    Patterns

---
+++ Patterns/NumberRightAngle.java
@@ -0,0 +1,11 @@
+//Numbers right angle triangle
+class NumberRightAngle{
+	public static void main(String args[]){
+		for(int i=1;i<=5;i++){
+			for(int j=1;j<=i;j++){
+				System.out.print(j);
+			}
+			System.out.println(" ");
+		}
+	}
+}
