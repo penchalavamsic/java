dc5fd9c9724eeee987c6fd0c2e97fc0af6b077f5
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-09-14 23:40:53 +0530

    new changes

--- backup/PrintNumbers.java
+++ backup/PrintNumbers.java
@@ -1,7 +1,7 @@
 //Printing numbers from 10 to 110
 class PrintNumbers{
 	public static void main(String args[]){
-		for(int i=0;i<=110;i++){
+		for(int i=10;i<=110;i++){
 			System.out.print(i+" ");
 		}
 	}
