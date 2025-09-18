15a3bdec85557eb8b0847b57de8a67ac869e371e
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-09-18 14:41:36 +0530

    Deafult constructor

---
+++ practice/Constructors/DefaultConstructor.java
@@ -0,0 +1,9 @@
+//Default COnstructor
+class Hello{
+	Hello(){		//Default constructor
+		System.out.println("Hello World"); 
+	}	
+	public static void main(String [] args){
+		Hello h=new Hello();
+	}
+}
