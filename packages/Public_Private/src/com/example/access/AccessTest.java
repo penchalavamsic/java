535fccb76dd0431c2d6a6d7714b7cae1b52c1c1e
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-09-22 23:03:24 +0530

    Public & Private classes access

---
+++ packages/Public_Private/src/com/example/access/AccessTest.java
@@ -0,0 +1,12 @@
+package com.example.access;
+
+public class AccessTest {
+	public static void main(String[] args) {
+		
+	
+	PublicClass pb=new PublicClass();
+	pb.showPublicMessage();
+//	PackagePrivateClass pv=new PackagePrivateClass();
+//	pv.showPackagePrivateMessage(); cannot access because of private
+	}
+}	
---
+++ packages/Public_Private/src/com/example/access/PublicClass.java
@@ -0,0 +1,8 @@
+package com.example.access;
+
+public class PublicClass {
+	public void showPublicMessage() {
+		System.out.println("This is a public class");
+	}
+}
+
