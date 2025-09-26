5e5f5e54e773fd0ed2d08711c73c11814ef941d1
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-09-26 21:26:51 +0530

    Using start() and run()

---
+++ MultiThreading/Cooking.java
@@ -0,0 +1,19 @@
+//Cooking food scenario with Multi Threading 
+//using run() and start()
+class CookingFood extends Thread{
+        String task;
+        public CookingFood(String task){
+            this.task=task;
+        }
+        public void run(){
+            System.out.println("Cooking "+task+"is done by"+Thread.currentThread().getName());
+        }
+}
+public class Cooking{
+    public static void main(String[] args) {
+        CookingFood c1=new CookingFood("Biryani");
+        CookingFood c2=new CookingFood("Noodles");
+        c1.start();
+        c2.start();
+    }
+}
