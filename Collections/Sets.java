452a9f15bf7fac668db5565a9c256c233820b9d8
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-10-11 23:17:59 +0530

    TreeSet with Ascending  order

--- Collections/Sets.java
+++ Collections/Sets.java
@@ -11,6 +11,7 @@ public class Sets{
         while(itr.hasNext()){
             System.out.println(itr.next());
         }
+        System.err.println("****");
 
         Set s1=new LinkedHashSet(); //with Insertion order
         s1.add(10);
@@ -19,6 +20,17 @@ public class Sets{
         Iterator itr1=s1.iterator();
         while(itr1.hasNext()){
             System.out.println(itr1.next());
+        }
+        System.out.println("****");
+        //Tree set
+        Set s2=new TreeSet();
+        s2.add(10);
+        s2.add(22);
+        s2.add(2); //gives ascending order by default
+        Iterator itr2=s2.iterator();
+        while(itr2.hasNext()){
+            System.out.println(itr2.next());
+
         }
     }
 }
