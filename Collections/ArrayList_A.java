e4b44b607d655495851bcb632a4a0f9075ba8e4b
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-10-11 18:08:13 +0530

    using iterator

--- Collections/ArrayList_A.java
+++ Collections/ArrayList_A.java
@@ -5,10 +5,21 @@ public class ArrayList_A {
         ArrayList<Integer> al=new ArrayList<Integer>(); //generics
         al.add(10);
         al.add(1);
+        al.add(56);
         System.out.println(al);
         Collections.sort(al); //sorting
         System.out.println(al);
-
+        System.out.println("****");
+        //normal for each
+        for(Object obj:al){
+            System.out.println(obj);// printing elements
+        }
+        System.out.println("****");
+        //using iterator
+        Iterator itr=al.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next()); //printing elements
+        }
 
     }
 }
