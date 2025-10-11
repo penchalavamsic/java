f963e77f117888937f90168148add99f3c77b8da
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-10-11 20:20:04 +0530

    User defined objects in List

---
+++ Collections/User_Objects.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class User_Objects{
+    public static class Student{
+        String name;
+        int rollno;
+        int age;
+        //constructor
+        Student(String name, int rollno, int age){
+            this.name=name;
+            this.rollno=rollno;
+            this.age=age;
+        }
+        //toString 
+        @Override
+        public String toString(){
+            return this.name+" "+this.rollno+" "+this.age;
+        }
+    }
+    public static void main(String[] args) {
+     List<Student> st=new ArrayList<Student>();
+     //user defined objects
+     st.add(new Student("A",1,23));   
+     st.add(new Student("B",2,24));   
+     st.add(new Student("C",3,25));   
+     System.out.println(st);
+    }
+}
