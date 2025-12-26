62748a75b5ec78d22a8c773867b5c393837573e7
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-12-26 21:56:55 +0530

    sorting by last digit by comparator

--- Collections/Comparator_Interface.java
+++ Collections/Comparator_Interface.java
@@ -1,13 +1,23 @@
+
 import java.util.*;
 
 class CompImpl implements Comparator<Integer> {
+
     @Override
-    public int compare(Integer o1, Integer o2) { 
+    public int compare(Integer o1, Integer o2) {
         return o1.compareTo(o2);
+    }
+
+    public int compare1(Integer o1, Integer o2) {
+        if (o1 % 10 > o2 % 10) { //finds the last digit of the number
+            return compare1(o1, o2);
+        }
+        return -1;
     }
 }
 
 public class Comparator_Interface {
+
     public static void main(String[] args) {
         List<Integer> values = new ArrayList<>();
         values.add(102);
@@ -16,7 +26,11 @@ public class Comparator_Interface {
         values.add(52);
 
         Comparator<Integer> c = new CompImpl();
-        Collections.sort(values, c);  
+        Collections.sort(values, c);
+        System.out.println(values);
+        Comparator<Integer> c1 = new CompImpl();
+        Collections.sort(values, c1);
         System.out.println(values);
     }
+
 }
