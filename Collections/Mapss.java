f898043037cd14624b7083f6bce1dbbbd40434af
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-12-27 22:14:06 +0530

    Map interface with HashMap

---
+++ Collections/Mapss.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Mapss {
+    public static void main(String[] args) {
+        Map<String, String> m=new HashMap<>();
+        m.put("1","A"); //we should pass key and value
+        m.put("2","B");
+        m.put("3","C");
+        for(String i:m.values()) //keyset returns the keys of the map
+            System.out.println(i);
+        
+    }
+}
